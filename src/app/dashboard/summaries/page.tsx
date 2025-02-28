bd8464105bf2b619790d8dd204f6b1f05a10094d
ArmandoHysaj <a.hysaj@ext.efg.gg>
2025-02-28 16:28:49 +0100

    last FE changes

--- src/app/dashboard/summaries/page.tsx
+++ src/app/dashboard/summaries/page.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import { getSummaries } from "@/data/loaders";
+import { Search } from "@/components/custom/Search";
+import { PaginationComponent } from "@/components/custom/PaginationComponent";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface LinkCardProps {
-  documentId: string;
+  id: string;
   title: string;
   summary: string;
 }
 
-function LinkCard({ documentId, title, summary }: Readonly<LinkCardProps>) {
+function LinkCard({ id, title, summary }: Readonly<LinkCardProps>) {
   return (
-    <Link href={`/dashboard/summaries/${documentId}`}>
+    <Link href={`/dashboard/summaries/${id}`}>
       <Card className="relative">
         <CardHeader>
           <CardTitle className="leading-8 text-pink-500">
@@ -28,16 +30,32 @@ function LinkCard({ documentId, title, summary }: Readonly<LinkCardProps>) {
   );
 }
 
-export default async function SummariesRoute() {
-  const { data } = await getSummaries();
+interface SearchParamsProps {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}
+
+export default async function SummariesRoute({
+  searchParams,
+}: Readonly<SearchParamsProps>) {
+  const query = searchParams?.query ?? "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const { data, meta } = await getSummaries(query, currentPage);
+  const pageCount = meta.pagination.pageCount;
+
   if (!data) return null;
   return (
     <div className="grid grid-cols-1 gap-4 p-4">
+      <Search />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data.map((item: LinkCardProps) => (
-          <LinkCard key={item.documentId} {...item} />
+          <LinkCard key={item.id} {...item} />
         ))}
       </div>
+      <PaginationComponent pageCount={pageCount} />
     </div>
   );
 }
